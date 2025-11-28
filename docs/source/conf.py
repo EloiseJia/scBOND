d9e76941f54c7b8530a7b4b12ada950ad6401127
EloiseJia <jiachenyang.sunny@gmail.com>
2025-11-28 10:36:50 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath(__file__+'../../../..'))
 
-import scBOND
+import scBond
 # -- Project information -----------------------------------------------------
 
 project = 'scBOND'
